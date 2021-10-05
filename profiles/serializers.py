e6740bfd3ef3c7d25b922c570e4db0f4127987ac
Wesley Mendes <wesley_gto_x@yahoo.com.br>
2021-10-05 19:48:42 -0300

    feat: adding endpoint to create a complete user profile

--- profiles/serializers.py
+++ profiles/serializers.py
@@ -1,6 +1,7 @@
 from django.contrib.auth import get_user_model
 from rest_framework import serializers
 
+from users.serializers import UserSerializer
 from profiles.models import Profile, SocialLink
 
 User = get_user_model()
@@ -23,10 +24,10 @@ class ProfileSerializer(serializers.ModelSerializer):
     location = serializers.SerializerMethodField()
     social_link = SocialLinkSerializer()
 
-    def get_name(self, obj):
+    def get_name(self, obj):  # noqa
         return str(obj)
 
-    def get_location(self, obj):
+    def get_location(self, obj):  # noqa
         return obj.get_location
 
     class Meta:
@@ -42,3 +43,31 @@ class ProfileSerializer(serializers.ModelSerializer):
             'social_link',
         )
         read_only_fields = ('uuid',)
+
+
+class ProfileCreateSerializer(serializers.ModelSerializer):
+    user = UserSerializer()
+    social_link = SocialLinkSerializer()
+
+    class Meta:
+        model = Profile
+        lookup_field = 'uuid'
+        fields = (
+            'user',
+            'occupation',
+            'contact_email',
+            'phone',
+            'city',
+            'country',
+            'social_link',
+        )
+
+    def create(self, validated_data):
+        user_data = validated_data.pop('user')
+        social_link_data = validated_data.pop('social_link')
+
+        user = User.objects.create_user(**user_data)
+        social_link = SocialLink.objects.create(**social_link_data)
+
+        profile = Profile.objects.create(user=user, social_link=social_link, **validated_data)
+        return profile
