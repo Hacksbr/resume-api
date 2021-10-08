b6dd268a1ad9db0b62d117c9a3bf2f675492cdff
Wesley Mendes <wesley_gto_x@yahoo.com.br>
2021-10-08 00:18:26 -0300

    test: users viewsets

--- users/tests/test_models.py
+++ users/tests/test_models.py
@@ -1,15 +1,14 @@
 from django.test import TestCase
 from django.contrib.auth import get_user_model
 
-from users.tests.fixture import UserFactory
+User = get_user_model()
 
 
 class UsersManagersTests(TestCase):
 
     def test_create_user(self) -> None:
-        User = get_user_model()
-        user = User.objects.create_user(email='normal@user.com', password='foo')
-        self.assertEqual(user.email, 'normal@user.com')
+        user = User.objects.create_user(email='user@user.com', password='foo')
+        self.assertEqual(user.email, 'user@user.com')
         self.assertTrue(user.is_active)
         self.assertFalse(user.is_staff)
         self.assertFalse(user.is_superuser)
@@ -26,11 +25,10 @@ class UsersManagersTests(TestCase):
             User.objects.create_user(email='')
 
         with self.assertRaises(ValueError):
-            User.objects.create_user(email='', password="foo")
+            User.objects.create_user(email='', password='foo')
 
     def test_create_superuser(self) -> None:
-        User = get_user_model()
-        admin_user = User.objects.create_superuser('super@user.com', 'foo')
+        admin_user = User.objects.create_superuser(email='super@user.com', password='foo')
         self.assertEqual(admin_user.email, 'super@user.com')
         self.assertTrue(admin_user.is_active)
         self.assertTrue(admin_user.is_staff)
@@ -45,6 +43,6 @@ class UsersManagersTests(TestCase):
             User.objects.create_superuser(email='super@user.com', password='foo', is_superuser=False)
 
     def test_user_attr(self) -> None:
-        user = UserFactory.create(first_name='Bruce', last_name='Wayne')
+        user = User.objects.create_user(first_name='Bruce', last_name='Wayne', email='bruce@wayne.we', password='foo')
         self.assertEqual(f'{user.first_name} {user.last_name}', user.get_full_name)
         self.assertEqual(user.first_name, user.get_short_name)
