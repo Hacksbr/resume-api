9f59f492e284f358210ef79f5a61d38b6580d4b6
Wesley Mendes <wesley_gto_x@yahoo.com.br>
2021-10-08 01:21:09 -0300

    test: fix auth token tests

--- api/tests/test_auth.py
+++ api/tests/test_auth.py
@@ -1,3 +1,5 @@
+import re
+
 from django.urls import reverse
 from django.contrib.auth import get_user_model
 
@@ -11,34 +13,23 @@ class AuthTokenTests(APITestCase):
 
     def setUp(self):
         self.data = {'email': 'bruce@user.com', 'password': 'foo'}
-        self.user = User.objects.create_user(
-            email=self.data['email'],
-            password=self.data['password']
-        )
+        self.user = User.objects.create_user(email=self.data.get('email'), password=self.data.get('password'))
+
+        self.regex = r'^[a-zA-Z0-9-_=]+\.[a-zA-Z0-9-_=]+\.?[a-zA-Z0-9-_.+/=]*'
 
     def test_token_login(self):
         # post to get token
-        response = self.client.post(
-            reverse('token_obtain_pair'),
-            data=self.data,
-            format='json'
-        )
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.assertTrue('access', response.data)
-        self.assertTrue('refresh', response.data)
+        response = self.client.post(reverse('token_obtain_pair'), data=self.data, format='json')
+        self.assertEqual(status.HTTP_200_OK, response.status_code)
+        self.assertTrue(bool(re.search(self.regex, response.data.get('access'))))
+        self.assertTrue(bool(re.search(self.regex, response.data.get('refresh'))))
 
     def test_token_refresh(self):
         # post to get token
-        response = self.client.post(
-            reverse('token_obtain_pair'),
-            data=self.data,
-            format='json'
-        )
+        response = self.client.post(reverse('token_obtain_pair'), data=self.data, format='json')
+        data = {'refresh': response.data.get('refresh')}
+
         # post to get refresh token
-        response = self.client.post(
-            reverse('token_refresh'),
-            data={'refresh': response.data['refresh']},
-            format='json'
-        )
-        self.assertEqual(response.status_code, 200)
-        self.assertTrue('access', response.data)
+        response = self.client.post(reverse('token_refresh'), data=data, format='json')
+        self.assertEqual(status.HTTP_200_OK, response.status_code)
+        self.assertTrue(bool(re.search(self.regex, response.data.get('access'))))
