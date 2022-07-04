b7f708b3c02f5dd1b9a91561583b67911abaab88
Wesley Mendes <wesley_gto_x@yahoo.com.br>
2022-07-03 22:09:00 -0300

    test: adding informative comment

--- api/tests/test_auth.py
+++ api/tests/test_auth.py
@@ -11,25 +11,33 @@ User = get_user_model()
 
 class AuthTokenTests(APITestCase):
 
-    def setUp(self):
+    def setUp(self) -> None:
+        """
+        Initial setup to run the tests.
+        """
         self.data = {'email': 'bruce@user.com', 'password': 'foo'}
         self.user = User.objects.create_user(email=self.data.get('email'), password=self.data.get('password'))
 
+        # regex to validate the generated token
         self.regex = r'^[a-zA-Z0-9-_=]+\.[a-zA-Z0-9-_=]+\.?[a-zA-Z0-9-_.+/=]*'
 
-    def test_token_login(self):
-        # post to get token
+    def test_token_login(self) -> None:
+        """
+        Validate that the token is being returned correctly.
+        """
         response = self.client.post(reverse('token_obtain_pair'), data=self.data, format='json')
         self.assertEqual(status.HTTP_200_OK, response.status_code)
         self.assertTrue(bool(re.search(self.regex, response.data.get('access'))))
         self.assertTrue(bool(re.search(self.regex, response.data.get('refresh'))))
 
-    def test_token_refresh(self):
-        # post to get token
+    def test_token_refresh(self) -> None:
+        """
+        Validate that the token is being updated.
+        """
         response = self.client.post(reverse('token_obtain_pair'), data=self.data, format='json')
         data = {'refresh': response.data.get('refresh')}
 
-        # post to get refresh token
+        # get refresh token
         response = self.client.post(reverse('token_refresh'), data=data, format='json')
         self.assertEqual(status.HTTP_200_OK, response.status_code)
         self.assertTrue(bool(re.search(self.regex, response.data.get('access'))))
