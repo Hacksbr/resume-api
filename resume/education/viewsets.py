e75e27b594b1db8555e2e5bf60a2a2b61b2465ee
Wesley Mendes <wesley_gto_x@yahoo.com.br>
2023-10-05 00:39:14 -0300

    feat: adding languages viewsets and serializers

--- resume/education/viewsets.py
+++ resume/education/viewsets.py
@@ -2,11 +2,17 @@
 from rest_framework import viewsets
 
 # Internal imports
-from resume.education.models import Education
-from resume.education.serializers import EducationSerializer
+from resume.education.models import Education, Language
+from resume.education.serializers import EducationSerializer, LanguageSerializer
 
 
 class EducationViewSet(viewsets.ModelViewSet):
     queryset = Education.objects.all()
     serializer_class = EducationSerializer
+    permission_classes = []
+
+
+class LanguageViewSet(viewsets.ModelViewSet):
+    queryset = Language.objects.all()
+    serializer_class = LanguageSerializer
     permission_classes = []
