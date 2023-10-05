e1b0de36d6458990cdf1517a19805511faaf7bdf
Wesley Mendes <wesley_gto_x@yahoo.com.br>
2023-10-05 00:38:53 -0300

    feat: adding languages models and admin

--- resume/education/models.py
+++ resume/education/models.py
@@ -52,3 +52,27 @@ class EducationSkill(ModelBase):
         verbose_name = _('Education Skill')
         verbose_name_plural = _('Education Skills')
         ordering = ['order']
+
+
+class Language(ModelBase):
+    class LanguageLevels(models.TextChoices):
+        BASIC = ('basic', _('Elementary proficiency'))
+        INTERMEDIATE = ('intermediate', _('Limited working proficiency'))
+        ADVANCED = ('advanced', _('Professional working proficiency'))
+        FLUENT = ('fluent', _('Full professional proficiency'))
+        NATIVE = ('native', _('Native or bilingual proficiency'))
+
+    name = models.CharField(_('Name'), max_length=255, blank=False, null=False)
+    level = models.CharField(_('Level'), max_length=12, choices=LanguageLevels.choices, blank=False, null=False)
+
+    profile = models.ForeignKey(
+        'profiles.Profile', verbose_name='Profile', related_name='languages', on_delete=models.CASCADE
+    )
+
+    def __str__(self):
+        return f'{self.name} - {self.level}'
+
+    class Meta:
+        verbose_name = _('Language')
+        verbose_name_plural = _('Languages')
+        ordering = ['name']
